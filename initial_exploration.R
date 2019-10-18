c3c7159de21b6ba5851893241cae8ef1d88faa88
eehrsam <ehrsam45@gmail.com>
2019-10-17 21:35:23 -0400

    Adding library data. As well as unique counts

--- initial_exploration.R
+++ initial_exploration.R
@@ -3,3 +3,18 @@ library(data.table)
 
 
 my_streaming <- as.data.table(fromJSON("StreamingHistory.json"))
+
+names(my_streaming)
+str(my_streaming)
+
+my_streaming[,.N,by="artistName"][order(-N)]
+my_streaming[,.N,by="trackName"][order(-N)]
+my_streaming[,.(min(endTime),max(endTime))] # listening from August 20 to November 17
+
+my_streaming[msPlayed == min(msPlayed),.N,by="trackName"]
+
+
+my_library_tracks <- as.data.table(fromJSON("YourLibrary.json")$tracks)
+my_library_albums  <- as.data.table(fromJSON("YourLibrary.json")$albums)
+
+
